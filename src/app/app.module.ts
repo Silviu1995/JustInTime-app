274a6d53042c66135265f12a07dd233b35e71018
Silviu1995 <111465804+Silviu1995@users.noreply.github.com>
2023-08-18 15:45:38 +0300

    :first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductComponent } from './products/product/product.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProfileComponent,
+    RestaurantsComponent,
+    ShoppingCartComponent,
+    NavbarComponent,
+    ProductsComponent,
+    ProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/products/product/product.component.ts
@@ -0,0 +1,93 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent {
+
+  products = [
+    {
+      title: 'shit',
+      price: 5,
+      imgUrl:'https://i.stack.imgur.com/XGFw7m.jpg',
+      description: 'A short description of the shit'
+    },
+    {
+      title: 'shit',
+      price: 5,
+      imgUrl:'https://upload.wikimedia.org/wikipedia/commons/3/38/20200719_180910_Suduch_Pizza%2C_Pizzeria_Yoav_Tel_Aviv_anagoria.jpg',
+      description: 'A short description of the shit'
+    },
+    {
+      title: 'shit',
+      price: 5,
+      imgUrl:'https://i.stack.imgur.com/XGFw7m.jpg',
+      description: 'A short description of the shit'
+    },
+    {
+      title: 'shit',
+      price: 5,
+      imgUrl:'https://i.stack.imgur.com/XGFw7m.jpg',
+      description: 'A short description of the shit'
+    },
+    {
+      title: 'shit',
+      price: 5,
+      imgUrl:'https://i.stack.imgur.com/XGFw7m.jpg',
+      description: 'A short description of the shit'
+    },
+    {
+      title: 'shit',
+      price: 5,
+      imgUrl:'https://i.stack.imgur.com/XGFw7m.jpg',
+      description: 'A short description of the shit'
+    },
+    {
+      title: 'shit',
+      price: 5,
+      imgUrl:'https://i.stack.imgur.com/XGFw7m.jpg',
+      description: 'A short description of the shit'
+    },
+    {
+      title: 'shit',
+      price: 5,
+      imgUrl:'https://i.stack.imgur.com/XGFw7m.jpg',
+      description: 'A short description of the shit'
+    },
+    {
+      title: 'shit',
+      price: 5,
+      imgUrl:'https://i.stack.imgur.com/XGFw7m.jpg',
+      description: 'A short description of the shit'
+    },
+    {
+      title: 'shit',
+      price: 5,
+      imgUrl:'https://i.stack.imgur.com/XGFw7m.jpg',
+      description: 'A short description of the shit'
+    },
+    {
+      title: 'shit',
+      price: 5,
+      imgUrl:'https://i.stack.imgur.com/XGFw7m.jpg',
+      description: 'A short description of the shit'
+    },
+    {
+      title: 'shit',
+      price: 5,
+      imgUrl:'https://i.stack.imgur.com/XGFw7m.jpg',
+      description: 'A short description of the shit'
+    },
+    {
+      title: 'shit',
+      price: 5,
+      imgUrl:'https://i.stack.imgur.com/XGFw7m.jpg',
+      description: 'A short description of the shit'
+    },
+    
+  ]
+
+
+}
